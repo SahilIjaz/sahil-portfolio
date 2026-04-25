46417b0e52fe27c0c7a5f1efb9926850cdbadc63
SahilIjaz <hssahil2913@gmail.com>
2026-04-26 02:23:55 +0500

    lkjkljljljkj

--- components/ScrollAnimations.tsx
+++ components/ScrollAnimations.tsx
@@ -6,7 +6,6 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
-
 export function useScrollAnimation(
   triggerSelector: string,
   callback: (progress: number) => void,
